8de19e7018ca4cd4272721cafc2dd4f3baba9003
pjh445 <pjh445@gmail.com>
2019-03-07 19:46:36 +0900

    Add files via upload

---
+++ beginnerstep3/index.js
@@ -0,0 +1,11 @@
+﻿const express = require ( "express");
+const app = express ();
+const PORT = process.env.PORT;
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(PORT); //the server object listens on port 8080
---
+++ index.js
@@ -0,0 +1,13 @@
+﻿var express = require("express");
+var app = express();
+var port = process.env.PORT||3000;
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(port, function(){
+  console.log('Server On!');
+});
